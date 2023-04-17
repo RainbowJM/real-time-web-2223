43a45dac5842402a7c35089cf24ee7278dede64f
K3A101 <keisha.alexander@hva.nl>
2023-04-17 12:27:06 +0200

    ejs geinstalleerd

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const port = process.env.PORT || 4242
 
 app.use(express.static(path.resolve('public')));
 
+app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+
+// Routing bestand
+let appRoutes = require('./routes/routes');
+app.use('/', appRoutes);
 
 http.listen(port, () => {
     console.log(`Example app listening on  http://localhost:${port}`)
