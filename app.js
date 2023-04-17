677e592a82cffaf830a3f1d39af3f482bc3e33fa
Laiba Choudhry <laiba@hotmail.nl>
2023-04-17 12:27:42 +0200

    opzetttt

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const path = require('path');
-// const io = require('socket.io')(http);
+const io = require('socket.io')(http);
 const port = process.env.PORT || 4242
 
 app.use(express.static(path.resolve('public')));
 
 
+io.on("connection",(client)=>{
+    console.log('user connected');
+});
+
 http.listen(port, () => {
     console.log(`Example app listening on  http://localhost:${port}`)
-})
+});
+
