767d71f8b8eb3776c26aaa55f53768e2cacfe45f
K3A101 <keisha.alexander@hva.nl>
2023-04-17 11:39:01 +0200

    exprees server opgesteld en app basis structuur

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const path = require('path');
+// const io = require('socket.io')(http);
+const port = process.env.PORT || 4242
+
+app.use(express.static(path.resolve('public')));
+
+
+http.listen(port, () => {
+    console.log(`Example app listening on  http://localhost:${port}`)
+})
